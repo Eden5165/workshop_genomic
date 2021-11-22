5581457e0c19100373c853254cc5b9c2d7abfd89
yaelza94 <yaelzamir@mail.tau.ac.il>
2021-11-22 14:43:34 +0200

    functions bank

---
+++ functions_bank.py
@@ -0,0 +1,100 @@
+
+
+# Normalization
+def gene_exp_log_trans(df):
+    pass
+
+
+def ic50_log_trans(df):
+    pass
+
+
+def norm_per_sample(df):
+    pass
+
+
+def norm_per_col(df):
+    pass
+
+
+def eliminate_extreme_vals(df):
+    pass
+
+
+# Missing values
+def missing_vals_reg(df):
+    pass
+
+
+def missing_vals_method(df, method):
+    pass
+
+
+# Feature Selection
+def select_high_var_genes(df):
+    pass
+
+
+def regularization(df):
+    pass
+
+
+# Dimension reduction
+def pca(df):
+    pass
+
+
+def k_means_reduction(df):
+    pass
+
+
+# Regression per drug
+def reg_per_drug(df):
+    """
+
+    :param df:
+    :return: Dictionary as such: {drug_name: {reg_model1: obj, reg_model2: obj...}}
+    """
+    pass
+
+
+def linear_reg_per_drug(df):
+    pass
+
+
+def decision_tree_reg_per_drug(df):
+    pass
+
+
+def gradient_boosting_per_drug(df):
+    pass
+
+
+# Chained Multi-output Regression
+def chained_multi_reg(df):
+    pass
+
+
+# Drugs Clusters
+def get_drug_clusters(df):
+    pass
+
+
+# Task 2
+
+
+def predict_unlabeled(model1, unlabeled_df):
+    pass
+
+
+def predict_unlabeled_coreg(model1, unlabeled_df):
+    pass
+
+
+
+
+
+
+
+
+
