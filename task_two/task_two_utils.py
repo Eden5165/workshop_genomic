74c3037df1043e33f7a5510538287cca4678d9c2
edenhadad <edenhadad@3disg.com>
2021-12-06 18:43:41 +0200

    task two create tcga table

--- task_two/task_two_utils.py
+++ task_two/task_two_utils.py
@@ -1,7 +1,30 @@
 import pandas as pd
+import numpy as np
+from utils import general_utils
 
-def coreg_algo(genes_df, drug_df):
+def coreg_algo(beat_rna_df, tcga_rna_df, drug_df):
     """
-
     """
     pass
+
+def predict_tcga(beat_rna_df, tcga_rna_df, drug_df, pipeline_task_one):
+    """
+    param beat_rna_df: beat rna df after data prep - tranpose, possibly norm
+    param tcga_rna_df: tcga rna df after data prep - tranpose, possibly norm
+    param drug_df: drug df after data prep - fill missing + tranpose
+    pipeline_task_one: best pipeline from task 1
+    return tcga_drug_predict_df
+    """
+    model_predictor, not_selcted_features, dimention_red_model = pipeline_task_one(beat_rna_df, drug_df)
+    tcga_after_filterd_features = tcga_rna_df.loc[:, ~(tcga_rna_df.columns.isin(not_selcted_features))]
+    if dimention_red_model is not None:
+        tcga_rna_ready_predict =  dimention_red_model.transform(tcga_after_filterd_features)
+    else:
+        tcga_rna_ready_predict = tcga_after_filterd_features
+    tcga_drug_predict = model_predictor.predict()
+    tcga_drug_predict_df = general_utils.convert_predict_to_df(tcga_drug_predict, drug_df.columns, tcga_rna_df.index)
+
+    return tcga_drug_predict_df
+
+
+
