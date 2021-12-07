c8ff8dba6bb54651f64139b1d08e20e77bb607e7
edenhadad <edenhadad@3disg.com>
2021-12-07 11:28:13 +0200

    pca and task2

--- utils/dimension_reduction_utils.py
+++ utils/dimension_reduction_utils.py
@@ -6,30 +6,26 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
+from utils import data_prep_utils
+
+
 PRESERVE_COMP = 0.95
 
 # Dimension reduction
-def pca(genes_df):
+def pca_full_path(feature_df):
     """
-    
+    param feature_df: genes feature df such that rows are samples and normelized
+    return: pca model(berfore fiting) and the new features list
     """
-    X = genes_df.values
-    print(X.shape)
-
-    # Standardize - do we need it? -> check row/column standart
-    scaler =  StandardScaler() 
-    scaler.fit(X)
-    X_scaled= scaler.transform(X)
+    feature_df_norm = data_prep_utils.norm_df(feature_df)
 
     pca = PCA(n_components=PRESERVE_COMP, random_state = 2020)
+    pca.fit(feature_df_norm)
+    new_feature_after_pca = pca.transform(feature_df_norm)
+    return pca, new_feature_after_pca
 
-    pca.fit(X_scaled)
-    X_pca = pca.transform(X_scaled)
-
-    print(X_pca.shape)
-    return pca, X_pca
-
-
+def get_pca_model(feature_df):
+    return PCA(n_components=PRESERVE_COMP, random_state = 2020)
 
 def aml_genes_selection(df):
     # Select only genes associated with AML as features
